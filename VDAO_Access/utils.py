6a6ce75d1f98e523b2c08fafd16f36e4f177b8fb
rafaelpadilla <rafael.padilla@smt.ufrj.br>
2018-03-17 21:26:50 -0300

    adding useful functions

--- VDAO_Access/utils.py
+++ VDAO_Access/utils.py
@@ -1,6 +1,7 @@
 import os
 import fnmatch
 import cv2
+import itertools
 
 # Ex: 
 # in: '/home/rafael/thesis/simulations/data1/test_data/000001.jpg'
@@ -10,6 +11,23 @@ def splitPathFile(fileDataPath):
     p = fileDataPath[:idx+1] #path
     f = fileDataPath[idx+1:] #file
     return p,f
+
+# Ex: 
+# in: '/home/rafael/thesis/simulations/data1/test_data/'
+# out: '{ 'home', 'rafael', 'thesis', 'simulations', 'data1', 'test_data' }
+def splitPaths(path):
+    folders = []
+    indexes = [i for i, letter in enumerate(path) if letter == '/']
+    for i in range(len(indexes)):
+        if i+1 < len(indexes):
+            item = path[indexes[i]:indexes[i+1]]
+        else:
+            item = path[indexes[i]:]
+        item = item.replace('/','')
+        if item != '':
+            folders.append(item)
+    return folders
+
 
 def getAllFilesRecursively(filePath, extension="*"):
     files = [os.path.join(dirpath, f)
@@ -29,6 +47,31 @@ def boxesIntersect(boxA, boxB):
     if boxA[1] > boxB[3]:
         return False # boxA is below boxB
     return True
+
+# box = (Ax1,Ay1,Ax2,Ay2)
+def getArea(box):
+    return (box[2] - box[0]) * (box[3] - box[1])
+
+def getNonOverlappedBoxes(boxes):
+    if len(boxes) == 1 or boxes == []:
+        return boxes, [0]
+
+    nonOverlappedBoxes = []
+    idxes = []
+    # Get combination among all boxes
+    combinations = list(itertools.combinations(boxes,2))
+    # Loop through the pairs
+    for combination in combinations:
+        # If boxes do not intersect
+        if boxesIntersect(combination[0], combination[1]) == False:
+            if combination[0] not in nonOverlappedBoxes:
+                nonOverlappedBoxes.append(combination[0])
+                idxes.append(boxes.index(combination[0]))
+            if combination[1] not in nonOverlappedBoxes:
+                nonOverlappedBoxes.append(combination[1])
+                idxes.append(boxes.index(combination[1]))
+    return nonOverlappedBoxes, idxes
+    
 
 def add_bb_into_image(image, boundingBox, color, thickness, label=None):
     r = int(color[0])
