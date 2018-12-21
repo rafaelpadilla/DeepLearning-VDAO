ab109bf9d9675a444717f56e0b2fa5a1189d3817
Rafael Padilla UFRJ <rafael.padilla@smt.ufrj.br>
2018-12-21 13:48:13 -0200

    Adding file with research videos (for training) and renaming object videos (for testing)

---
+++ download_vdao_research.sh
@@ -0,0 +1,348 @@
+############
+# TABLE 01 #
+############
+mkdir Table_01 && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/ref-video01.avi -P Table_01/ && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/obj-video01.avi -P Table_01/ && 
+############
+# TABLE 02 #
+############
+mkdir Table_02 && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/ref-video02.avi -P Table_02/ && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/obj-video02.avi -P Table_02/ && 
+############
+# TABLE 03 #
+############
+mkdir Table_03 && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/ref-video03.avi -P Table_03/ && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/obj-video03.avi -P Table_03/ && 
+############
+# TABLE 04 #
+############
+mkdir Table_04 && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/ref-video04.avi -P Table_04/ && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/obj-video04.avi -P Table_04/ && 
+############
+# TABLE 05 #
+############
+mkdir Table_05 && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/ref-video05.avi -P Table_05/ && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/obj-video05.avi -P Table_05/ && 
+############
+# TABLE 06 #
+############
+mkdir Table_06 && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/ref-video06.avi -P Table_06/ && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/obj-video06.avi -P Table_06/ && 
+############
+# TABLE 07 #
+############
+mkdir Table_07 && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/ref-video07.avi -P Table_07/ && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/obj-video07.avi -P Table_07/ && 
+############
+# TABLE 08 #
+############
+mkdir Table_08 && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/ref-video08.avi -P Table_08/ && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/obj-video08.avi -P Table_08/ && 
+############
+# TABLE 09 #
+############
+mkdir Table_09 && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/ref-video09.avi -P Table_09/ && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/obj-video09.avi -P Table_09/ && 
+############
+# TABLE 10 #
+############
+mkdir Table_10 && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/ref-video10.avi -P Table_10/ && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/obj-video10.avi -P Table_10/ && 
+############
+# TABLE 11 #
+############
+mkdir Table_11 && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/ref-video11.avi -P Table_11/ && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/obj-video11.avi -P Table_11/ && 
+############
+# TABLE 12 #
+############
+mkdir Table_12 && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/ref-video12.avi -P Table_12/ && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/obj-video12.avi -P Table_12/ && 
+############
+# TABLE 13 #
+############
+mkdir Table_13 && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/ref-video13.avi -P Table_13/ && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/obj-video13.avi -P Table_13/ && 
+############
+# TABLE 14 #
+############
+mkdir Table_14 && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/ref-video14.avi -P Table_14/ && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/obj-video14.avi -P Table_14/ && 
+############
+# TABLE 15 #
+############
+mkdir Table_15 && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/ref-video15.avi -P Table_15/ && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/obj-video15.avi -P Table_15/ && 
+############
+# TABLE 16 #
+############
+mkdir Table_16 && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/ref-video16.avi -P Table_16/ && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/obj-video16.avi -P Table_16/ && 
+############
+# TABLE 17 #
+############
+mkdir Table_17 && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/ref-video17.avi -P Table_17/ && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/obj-video17.avi -P Table_17/ && 
+############
+# TABLE 18 #
+############
+mkdir Table_18 && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/ref-video18.avi -P Table_18/ && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/obj-video18.avi -P Table_18/ && 
+############
+# TABLE 19 #
+############
+mkdir Table_19 && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/ref-video19.avi -P Table_19/ && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/obj-video19.avi -P Table_19/ && 
+############
+# TABLE 20 #
+############
+mkdir Table_20 && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/ref-video20.avi -P Table_20/ && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/obj-video20.avi -P Table_20/ && 
+############
+# TABLE 21 #
+############
+mkdir Table_21 && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/ref-video21.avi -P Table_21/ && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/obj-video21.avi -P Table_21/ && 
+############
+# TABLE 22 #
+############
+mkdir Table_22 && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/ref-video22.avi -P Table_22/ && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/obj-video22.avi -P Table_22/ && 
+############
+# TABLE 23 #
+############
+mkdir Table_23 && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/ref-video23.avi -P Table_23/ && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/obj-video23.avi -P Table_23/ && 
+############
+# TABLE 24 #
+############
+mkdir Table_24 && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/ref-video24.avi -P Table_24/ && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/obj-video24.avi -P Table_24/ && 
+############
+# TABLE 25 #
+############
+mkdir Table_25 && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/ref-video25.avi -P Table_25/ && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/obj-video25.avi -P Table_25/ && 
+############
+# TABLE 26 #
+############
+mkdir Table_26 && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/ref-video26.avi -P Table_26/ && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/obj-video26.avi -P Table_26/ && 
+############
+# TABLE 27 #
+############
+mkdir Table_27 && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/ref-video27.avi -P Table_27/ && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/obj-video27.avi -P Table_27/ && 
+############
+# TABLE 28 #
+############
+mkdir Table_28 && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/ref-video28.avi -P Table_28/ && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/obj-video28.avi -P Table_28/ && 
+############
+# TABLE 29 #
+############
+mkdir Table_29 && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/ref-video29.avi -P Table_29/ && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/obj-video29.avi -P Table_29/ && 
+############
+# TABLE 30 #
+############
+mkdir Table_30 && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/ref-video30.avi -P Table_30/ && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/obj-video30.avi -P Table_30/ && 
+############
+# TABLE 31 #
+############
+mkdir Table_31 && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/ref-video31.avi -P Table_31/ && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/obj-video31.avi -P Table_31/ && 
+############
+# TABLE 32 #
+############
+mkdir Table_32 && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/ref-video32.avi -P Table_32/ && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/obj-video32.avi -P Table_32/ && 
+############
+# TABLE 33 #
+############
+mkdir Table_33 && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/ref-video33.avi -P Table_33/ && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/obj-video33.avi -P Table_33/ && 
+############
+# TABLE 34 #
+############
+mkdir Table_34 && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/ref-video34.avi -P Table_34/ && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/obj-video34.avi -P Table_34/ && 
+############
+# TABLE 35 #
+############
+mkdir Table_35 && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/ref-video35.avi -P Table_35/ && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/obj-video35.avi -P Table_35/ && 
+############
+# TABLE 37 #
+############
+mkdir Table_37 && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/ref-video37.avi -P Table_37/ && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/obj-video37.avi -P Table_37/ && 
+############
+# TABLE 38 #
+############
+mkdir Table_38 && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/ref-video38.avi -P Table_38/ && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/obj-video38.avi -P Table_38/ && 
+############
+# TABLE 39 #
+############
+mkdir Table_39 && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/ref-video39.avi -P Table_39/ && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/obj-video39.avi -P Table_39/ && 
+############
+# TABLE 40 #
+############
+mkdir Table_40 && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/ref-video40.avi -P Table_40/ && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/obj-video40.avi -P Table_40/ && 
+############
+# TABLE 41 #
+############
+mkdir Table_41 && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/ref-video41.avi -P Table_41/ && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/obj-video41.avi -P Table_41/ && 
+############
+# TABLE 42 #
+############
+mkdir Table_42 && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/ref-video42.avi -P Table_42/ && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/obj-video42.avi -P Table_42/ && 
+############
+# TABLE 43 #
+############
+mkdir Table_43 && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/ref-video43.avi -P Table_43/ && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/obj-video43.avi -P Table_43/ && 
+############
+# TABLE 44 #
+############
+mkdir Table_44 && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/ref-video44.avi -P Table_44/ && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/obj-video44.avi -P Table_44/ && 
+############
+# TABLE 45 #
+############
+mkdir Table_45 && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/ref-video45.avi -P Table_45/ && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/obj-video45.avi -P Table_45/ && 
+############
+# TABLE 46 #
+############
+mkdir Table_46 && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/ref-video46.avi -P Table_46/ && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/obj-video46.avi -P Table_46/ && 
+############
+# TABLE 47 #
+############
+mkdir Table_47 && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/ref-video47.avi -P Table_47/ && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/obj-video47.avi -P Table_47/ && 
+############
+# TABLE 48 #
+############
+mkdir Table_48 && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/ref-video48.avi -P Table_48/ && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/obj-video48.avi -P Table_48/ && 
+############
+# TABLE 49 #
+############
+mkdir Table_49 && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/ref-video49.avi -P Table_49/ && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/obj-video49.avi -P Table_49/ && 
+############
+# TABLE 50 #
+############
+mkdir Table_50 && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/ref-video50.avi -P Table_50/ && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/obj-video50.avi -P Table_50/ && 
+############
+# TABLE 51 #
+############
+mkdir Table_51 && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/ref-video51.avi -P Table_51/ && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/obj-video51.avi -P Table_51/ && 
+############
+# TABLE 52 #
+############
+mkdir Table_52 && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/ref-video52.avi -P Table_52/ && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/obj-video52.avi -P Table_52/ && 
+############
+# TABLE 53 #
+############
+mkdir Table_53 && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/ref-video53.avi -P Table_53/ && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/obj-video53.avi -P Table_53/ && 
+############
+# TABLE 54 #
+############
+mkdir Table_54 && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/ref-video54.avi -P Table_54/ && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/obj-video54.avi -P Table_54/ && 
+############
+# TABLE 55 #
+############
+mkdir Table_55 && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/ref-video55.avi -P Table_55/ && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/obj-video55.avi -P Table_55/ && 
+############
+# TABLE 56 #
+############
+mkdir Table_56 && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/ref-video56.avi -P Table_56/ && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/obj-video56.avi -P Table_56/ && 
+############
+# TABLE 57 #
+############
+mkdir Table_57 && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/ref-video57.avi -P Table_57/ && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/obj-video57.avi -P Table_57/ && 
+############
+# TABLE 58 #
+############
+mkdir Table_58 && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/ref-video58.avi -P Table_58/ && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/obj-video58.avi -P Table_58/ && 
+############
+# TABLE 59 #
+############
+mkdir Table_59 && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/ref-video59.avi -P Table_59/ && 
+wget http://www02.smt.ufrj.br/~tvdigital/database/research/data/avi/obj-video59.avi -P Table_59/ 
