e315b64b2bfb61cd5517615b504c63cdd56953dc
Rafael Padilla UFRJ <rafael.padilla@smt.ufrj.br>
2019-03-07 02:30:26 -0300

    Adding TP FP and DIS. Adding a summary_results in the dictionary.

--- ICIP_replication/Grow_RandomForest.py
+++ ICIP_replication/Grow_RandomForest.py
@@ -11,7 +11,7 @@ from sklearn.metrics import accuracy_score
 random.seed(0)
 np.random.seed(0)
 
-BASE_DIR = '/media/storage/VDAO/'
+BASE_DIR = '/nfs/proc/rafael.padilla'
 features_dir = { 'train': os.path.join(BASE_DIR,'vdao_alignment_object/shortest_distance/features/'),
                  'test': os.path.join(BASE_DIR,'vdao_alignment_research/shortest_distance/features/') }
 JSON_FILE_RESEARCH = 'vdao_research.json'
@@ -54,12 +54,6 @@ def get_predict_probabilities(predict_proba, list_name_features, classes=[0,1]):
         ret[list_name_features[id]] = {str(classe): predict_proba[id][classe] for classe in classes}
     return ret
 
-# def test_RF_classifier(rf_classifier, )
-#     # Check accuracy on training data
-#     y_pred = rnd_clf.predict(X)
-#     accuracy = accuracy_score(Y, y_pred)
-
-
 def get_features(_features_dir, max_features_per_class = None, shuffle=True, balance_classes=True):
     # Get paths of negative samples
     neg_path = os.path.join(_features_dir,'neg')
@@ -96,17 +90,19 @@ def get_features(_features_dir, max_features_per_class = None, shuffle=True, bal
     Y = np.array([0]*len(negative_npys_paths)+[1]*len(positive_npys_paths))
     return X, Y, paths_features
 
-target_objects = ['shoe', 'towel', 'brown box', 'black coat', 'black backpack', 'dark-blue box', 'camera box', 'white jar', 'pink bottle']
+target_objects = ['shoe', 'towel', 'brown box', 'black coat', 'black backpack', 'dark blue box', 'camera box', 'white jar', 'pink bottle']
 folds = {'fold_1': target_objects[0],
-                'fold_2': target_objects[1],
-                'fold_3': target_objects[2],
-                'fold_4': target_objects[3],
-                'fold_5': target_objects[4],
-                'fold_6': target_objects[5],
-                'fold_7': target_objects[6],
-                'fold_8': target_objects[7],
-                'fold_9': target_objects[8]
+         'fold_2': target_objects[1],
+         'fold_3': target_objects[2],
+         'fold_4': target_objects[3],
+         'fold_5': target_objects[4],
+         'fold_6': target_objects[5],
+         'fold_7': target_objects[6],
+         'fold_8': target_objects[7],
+         'fold_9': target_objects[8]
 }
+
+n_estimators = 10
 
 layers = ['conv1','residual1','residual2','residual3','residual4','residual5',
           'residual6','residual7','residual8','residual9','residual10','residual11',
@@ -118,6 +114,10 @@ for fold in folds:
     tar_obj = folds[fold]
     # Obtain target objects info into the testing structure
     test_object_info = get_objects_info([tar_obj], JSON_FILE_RESEARCH)
+    # Calculate TP and FP for the whole layer considering all tables (videos)
+    all_TP = 0
+    all_FP = 0
+    all_DIS = 0
     # Loop through every layer
     for layer in layers:
         # Print out status for new training
@@ -134,7 +134,7 @@ for fold in folds:
         amount_pos = (Y_hat == 1).sum()
         amount_neg = (Y_hat == 0).sum()
         # Apply random forest classification
-        rnd_clf = RandomForestClassifier()
+        rnd_clf = RandomForestClassifier(n_estimators=n_estimators)
         rnd_clf.fit(X,Y_hat)
         # Predict the training data (validation)
         Y_pred = rnd_clf.predict(X)
@@ -171,7 +171,7 @@ for fold in folds:
                 print(f'Testing folder {dir_features_test} not found.')
                 continue
             # Get features for testing
-            X_test, Y_test_hat, paths_feat_testing = get_features(dir_features_test, max_features_per_class=None, shuffle=True, balance_classes=False)
+            X_test, Y_test_hat, paths_feat_testing = get_features(dir_features_test, max_features_per_class=None, shuffle=False, balance_classes=False)
             # Get amount of positives and negatives
             amount_pos = (Y_test_hat == 1).sum()
             amount_neg = (Y_test_hat == 0).sum()
@@ -184,19 +184,55 @@ for fold in folds:
             print('Dataset distribution (pos, neg): ({}, {}) : ({:.1%}, {:.1%})'.format(amount_pos,amount_neg,amount_pos/len(Y_test_hat),amount_neg/len(Y_test_hat)))
             print('Accuracy: {:.2%}'.format(accuracy))
             # Add the results in the dictionary
+            final_results = {}
+            TP = 0
+            FP = 0
+            for idx in range(len(paths_feat_testing)):
+                final_results[paths_feat_testing[idx]] = {}
+                final_results[paths_feat_testing[idx]]['groundtruth_class'] = int('pos' in paths_feat_testing[idx])
+                assert int(Y_test_hat[idx]) == int('pos' in paths_feat_testing[idx]) # double checking
+                final_results[paths_feat_testing[idx]]['predicted_class'] = int(Y_test_pred[idx])
+                assert int(Y_test_pred[idx]) == int(rnd_clf.predict(X_test[idx].reshape(1,-1))) # double checking
+                final_results[paths_feat_testing[idx]]['predicted_correcly'] = final_results[paths_feat_testing[idx]]['groundtruth_class'] == final_results[paths_feat_testing[idx]]['predicted_class']
+                # If an object was predicted in the scene
+                if final_results[paths_feat_testing[idx]]['predicted_class']  == 1:
+                    # Predicted an object and there was an object
+                    if final_results[paths_feat_testing[idx]]['groundtruth_class'] == 1:
+                        TP += 1
+                    # Predicted an object but there was no object
+                    else:
+                        FP += 1
+            print('TP: %d' % TP)
+            print('FP: %d' % FP)
+            DIS = np.sqrt((1-TP)**2 + FP**2)
+            print('DIS: %.2f' % DIS)
+            # Update FP and TP for the currenct layer
+            all_TP += TP
+            all_FP += FP
             results_table = {}
             results_table[table] = compare_predictions(Y_test_hat, Y_test_pred)
             results_table[table]['correct_predict_features'] = [paths_feat_testing[id] for id in results_table[table]['correct_ids']]
             results_table[table]['incorrect_predict_features'] = [paths_feat_testing[id] for id in results_table[table]['incorrect_ids']]
             results_table[table]['predict_proba'] = get_predict_probabilities(rnd_clf.predict_proba(X_test), paths_feat_testing, classes=[0,1])
+            results_table[table]['summary_results'] = final_results
+            results_table[table]['TP'] = TP
+            results_table[table]['FP'] = FP
+            results_table[table]['DIS'] = DIS
             # Add table results to the result_testing dict
             results['result_testing'][table] = results_table[table]
         # Obtain the overall accuracy considering features of all tables
         # overall_accuracy = sum([results[t]['accuracy'] for t in results])/len(results)
         overall_accuracy = accuracy_score(overall_Y_test_hat, overall_Y_test_pred)
+        all_DIS = np.sqrt((1-all_TP)**2 + all_FP**2)
         results['result_testing']['overall_accuracy'] = overall_accuracy
+        results['result_testing']['overall_TP'] = all_TP
+        results['result_testing']['overall_FP'] = all_FP
+        results['result_testing']['overall_DIS'] = all_DIS
         print('-'*40)
         print('Overall accuracy: {:.2%}'.format(overall_accuracy))
+        print('Overall TP: %d' % all_TP)
+        print('Overall FP: %d' % all_FP)
+        print('Overall DIS: %.2f' % all_DIS)
         # Save results
         dir_save_results = f'RF_results/{fold}'
         if not os.path.isdir(dir_save_results):
@@ -206,6 +242,6 @@ for fold in folds:
         pickle.dump(results,f)
         f.close()
         print('Saving results in: %s' % path_save_results)
+
 print('Finished!')
 
-        
