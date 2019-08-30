79de3d7cfd4238bb75161d958ba7a445b39c6bbe
Rafael Padilla UFRJ <rafael.padilla@smt.ufrj.br>
2019-08-30 17:03:11 -0300

    Adiciona metodos para serem usados no PCA (de outro modulo). Também transforma em modulo

--- ICIP_replication/Generate_Features.py
+++ ICIP_replication/Generate_Features.py
@@ -6,7 +6,6 @@ import socket
 import time
 import warnings
 
-import cv2
 import numpy as np
 import torch
 import torchvision.transforms as transforms
@@ -15,6 +14,7 @@ from torch.autograd import Variable
 from torchsummary import summary
 
 import _init_paths_
+import cv2
 import My_Resnet
 from VDAOVideo import VDAOVideo
 
@@ -126,6 +126,134 @@ def get_sizes_features_vector(input_size,
     return feature_maps_sizes
 
 
+def registra_hooks(model, nome_layer, callback):
+
+    if 'conv1' == nome_layer:
+        model.add_hook('conv1', 'conv1', None, callback, {
+            'layer_name': 'conv1',
+            'pooling_layer': None
+        })
+    elif 'residual1' == nome_layer:
+        model.add_hook('residual1', 'layer1', '0', callback, {
+            'layer_name': 'residual1',
+            'pooling_layer': None
+        })
+    elif 'residual2' == nome_layer:
+        model.add_hook('residual2', 'layer1', '1', callback, {
+            'layer_name': 'residual2',
+            'pooling_layer': None
+        })
+    elif 'residual3' == nome_layer:
+        model.add_hook('residual3', 'layer1', '2', callback, {
+            'layer_name': 'residual3',
+            'pooling_layer': None
+        })
+    elif 'residual4' == nome_layer:
+        model.add_hook('residual4', 'layer2', '0', callback, {
+            'layer_name': 'residual4',
+            'pooling_layer': None
+        })
+    elif 'residual5' == nome_layer:
+        model.add_hook('residual5', 'layer2', '1', callback, {
+            'layer_name': 'residual5',
+            'pooling_layer': None
+        })
+    elif 'residual6' == nome_layer:
+        model.add_hook('residual6', 'layer2', '2', callback, {
+            'layer_name': 'residual6',
+            'pooling_layer': None
+        })
+    elif 'residual7' == nome_layer:
+        model.add_hook('residual7', 'layer2', '3', callback, {
+            'layer_name': 'residual7',
+            'pooling_layer': None
+        })
+    elif 'residual8' == nome_layer:
+        model.add_hook('residual8', 'layer3', '0', callback, {
+            'layer_name': 'residual8',
+            'pooling_layer': None
+        })
+    elif 'residual9' == nome_layer:
+        model.add_hook('residual9', 'layer3', '1', callback, {
+            'layer_name': 'residual9',
+            'pooling_layer': None
+        })
+    elif 'residual10' == nome_layer:
+        model.add_hook('residual10', 'layer3', '2', callback, {
+            'layer_name': 'residual10',
+            'pooling_layer': None
+        })
+    elif 'residual11' == nome_layer:
+        model.add_hook('residual11', 'layer3', '3', callback, {
+            'layer_name': 'residual11',
+            'pooling_layer': None
+        })
+    elif 'residual12' == nome_layer:
+        model.add_hook('residual12', 'layer3', '4', callback, {
+            'layer_name': 'residual12',
+            'pooling_layer': None
+        })
+    elif 'residual13' == nome_layer:
+        model.add_hook('residual13', 'layer3', '5', callback, {
+            'layer_name': 'residual13',
+            'pooling_layer': None
+        })
+    elif 'residual14' == nome_layer:
+        model.add_hook('residual14', 'layer4', '0', callback, {
+            'layer_name': 'residual14',
+            'pooling_layer': None
+        })
+    elif 'residual15' == nome_layer:
+        model.add_hook('residual15', 'layer4', '1', callback, {
+            'layer_name': 'residual15',
+            'pooling_layer': None
+        })
+    elif 'residual16' == nome_layer:
+        model.add_hook('residual16', 'layer4', '2', callback, {
+            'layer_name': 'residual16',
+            'pooling_layer': None
+        })
+
+
+def get_features(model, device, frame, layer_to_extract, resize_input_factor=1, apply_post_pooling=False):
+    if next(model.parameters()).is_cuda is False:
+        model.to(device)
+
+    frame_height, frame_width, frame_channels = frame.shape[0], frame.shape[1], frame.shape[2]
+    size_frame = [frame_height, frame_width, frame_channels]
+    # Resize according to the factor
+    input_size = [
+        int(size_frame[0] / resize_input_factor),
+        int(size_frame[1] / resize_input_factor),
+        int(size_frame[2])
+    ]
+    # Define transformations to be used to extract features
+    resize_transform = transforms.Resize(input_size[:2])
+    normalize_transform = transforms.Normalize(mean=My_Resnet.mean_imagenet,
+                                               std=My_Resnet.std_imagenet)
+    to_tensor_transform = transforms.ToTensor()
+    transformations = transforms.Compose(
+        [resize_transform, to_tensor_transform, normalize_transform])
+
+    features_extracted = None
+
+    def features_extracted_callback(parameters):
+        def hook(m, i, o):
+            assert parameters['layer_name'] == layer_to_extract
+            features_extracted = o.data.squeeze().cpu()
+        return hook
+
+    if layer_to_extract not in model.hooks:
+        registra_hooks(model, layer_to_extract, features_extracted_callback)
+
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    # Transform image and pass it throught the network
+    image = Image.fromarray(np.uint8(frame))
+    t_img = torch.tensor(transformations(image).unsqueeze(0), dtype=torch.float, device=device)
+    model(t_img)
+    a = 123
+
+
 def generate_and_save_features(video_path,
                                dir_to_save_features,
                                prefix_file_name,
@@ -161,7 +289,7 @@ def generate_and_save_features(video_path,
                 '#frameNumber#', str(frame_num - 1))
             feat_path = os.path.join(dir_to_save_features, parameters['layer_name'], feat_path)
             # Make sure the feature was not created yet que a feature ainda não foi gerada
-            assert os.path.isfile(feat_path) == False
+            assert os.path.isfile(feat_path) is False
             # Save (we must copy the tensor to the cpu, thats why we use the .cpu() function)
             np.save(feat_path, feature_vector.cpu())
 
@@ -256,6 +384,7 @@ def generate_and_save_features(video_path,
                 'layer_name': 'residual16',
                 'pooling_layer': get_avg_poolings('residual16')
             })
+    # Registra hooks
     else:
         if 'conv1' in layers_to_extract:
             resnet50.add_hook('conv1', 'conv1', None, features_extracted_callback, {
@@ -359,91 +488,93 @@ def generate_and_save_features(video_path,
     resnet50.remove_all_hooks()
 
 
-# Load the resnet50 with pretrained model
-resnet50 = My_Resnet.resnet50(pretrained=True)
-# Define device
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-# summary(resnet50.to(device), input_size)
-# Set model to evaluation mode
-resnet50.to(device)
-resnet50.eval()
-# Get directories to read frames from and write feature maps
-dir_read, dir_save = define_folders()
+if __name__ == "__main__":
+    # Load the resnet50 with pretrained model
+    resnet50 = My_Resnet.resnet50(pretrained=True)
+    # Define device
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    # summary(resnet50.to(device), input_size)
+    # Set model to evaluation mode
+    resnet50.to(device)
+    resnet50.eval()
+    # Get directories to read frames from and write feature maps
+    dir_read, dir_save = define_folders()
 
-####################################################################################
-################################### Definitions ####################################
-####################################################################################
-# Change here to 'research' or 'object'
-database_type = 'object'
-# Change here 'shortest_distance' or 'dtw'
-alignment_mode = 'shortest_distance'
-layers_to_extract = [
-    'conv1', 'residual1', 'residual2', 'residual3', 'residual4', 'residual5', 'residual6',
-    'residual7', 'residual8', 'residual9', 'residual10', 'residual11', 'residual12', 'residual13',
-    'residual14', 'residual15', 'residual16'
-]
-layers_to_extract = ['residual3']
-tables_to_process = ['table %s' % l_double_digit(i) for i in range(1, 60)]
-# Parameters
-resize_factor = 2
-# Change here to True or False
-apply_pooling = False
-# Change here the name of the directory to save the features
-name_dir = 'no_pooling_features'
-start = time.time()
-print('Starting process at: %s' % start)
-print('Main parameters:')
-print('* resize_factor = %f' % resize_factor)
-print('* apply_pooling = %s' % apply_pooling)
+    ####################################################################################
+    ################################### Definitions ####################################
+    ####################################################################################
+    # Change here to 'research' or 'object'
+    database_type = 'object'
+    # Change here 'shortest_distance' or 'dtw'
+    alignment_mode = 'shortest_distance'
+    layers_to_extract = [
+        'conv1', 'residual1', 'residual2', 'residual3', 'residual4', 'residual5', 'residual6',
+        'residual7', 'residual8', 'residual9', 'residual10', 'residual11', 'residual12',
+        'residual13', 'residual14', 'residual15', 'residual16'
+    ]
+    layers_to_extract = ['residual3']
+    tables_to_process = ['table %s' % l_double_digit(i) for i in range(1, 2)]
+    # Parameters
+    resize_factor = 2
+    # Change here to True or False
+    apply_pooling = False
+    # Change here the name of the directory to save the features
+    name_dir = 'no_pooling_features'
+    start = time.time()
+    print('Starting process at: %s' % start)
+    print('Main parameters:')
+    print('* resize_factor = %f' % resize_factor)
+    print('* apply_pooling = %s' % apply_pooling)
 
-dir_videos = os.path.join(dir_read, 'vdao_%s/' % database_type)
-dir_save = os.path.join(dir_save, 'vdao_alignment_%s' % database_type, alignment_mode, name_dir)
-print('Folder to read videos: %s' % dir_videos)
-print('Folder to save features: %s' % dir_save)
+    dir_videos = os.path.join(dir_read, 'vdao_%s/' % database_type)
+    dir_save = os.path.join(dir_save, 'vdao_alignment_%s' % database_type, alignment_mode, name_dir)
+    print('Folder to read videos: %s' % dir_videos)
+    print('Folder to save features: %s' % dir_save)
 
-dir_save_features = os.path.join(dir_videos, )
-for table_name in tables_to_process:
-    print('\nGenerating features from table %s' % table_name)
-    # Get all videos within the dir_videos
-    print(os.path.join(dir_videos, table_name.replace(' ', '_'), '*', '*.avi'))
-    videos_paths = glob.glob(os.path.join(dir_videos, table_name.replace(' ', '_'), '*', '*.avi'))
-    # Loop through each video
-    for video_path in videos_paths:
-        # Depending on the video, it is a target or a reference
-        if '-Object_' in video_path:
-            tag = '-Object_'
-            type_feat = 'tar'
-        elif '-Reference_' in video_path:
-            tag = '-Reference_'
-            type_feat = 'ref'
-        # Get table number
-        table_number = table_name.replace('table ', '')
-        # Get Object/Reference video number
-        idx = video_path.index(tag)
-        object_number = video_path[idx + len(tag):]
-        idx = object_number.index('/')
-        object_number = object_number[:idx]
-        # Create dir chain to store the features of the current video
-        dirs = os.path.dirname(video_path).split('/')
-        idx = dirs.index(table_name.replace(' ', '_'))
-        dir_features_to_save = dir_save
-        for i in dirs[idx:]:
-            dir_features_to_save = os.path.join(dir_features_to_save, i)
-        # For each layer to extract, create directory structure to save the features of the current video
-        for layer_name in layers_to_extract:
-            dir_features_to_save_layer = os.path.join(dir_features_to_save, layer_name)
-            if not os.path.isdir(dir_features_to_save_layer):
-                os.makedirs(dir_features_to_save_layer)
-        # Name of the file with the feature is defined as: 'feat_[#name_of_the_video#]_#layer_name#_t#table_number#_#type_feature(ref or tar)#_vid#object_number#_frame#frame_number(starts at 0)#.npy'
-        prefix_file_name = f'feat_[%s]_#layerName#_t{table_number}_{type_feat}_vid{object_number}_frame_#frameNumber#.npy' % (
-            os.path.basename(video_path).replace('.avi', ''))
-        # Get features of the frame from all layers
-        generate_and_save_features(video_path,
-                                   dir_features_to_save,
-                                   prefix_file_name,
-                                   layers_to_extract,
-                                   resize_factor=resize_factor,
-                                   apply_post_pooling=apply_pooling)
+    dir_save_features = os.path.join(dir_videos, )
+    for table_name in tables_to_process:
+        print('\nGenerating features from table %s' % table_name)
+        # Get all videos within the dir_videos
+        print(os.path.join(dir_videos, table_name.replace(' ', '_'), '*', '*.avi'))
+        videos_paths = glob.glob(
+            os.path.join(dir_videos, table_name.replace(' ', '_'), '*', '*.avi'))
+        # Loop through each video
+        for video_path in videos_paths:
+            # Depending on the video, it is a target or a reference
+            if '-Object_' in video_path:
+                tag = '-Object_'
+                type_feat = 'tar'
+            elif '-Reference_' in video_path:
+                tag = '-Reference_'
+                type_feat = 'ref'
+            # Get table number
+            table_number = table_name.replace('table ', '')
+            # Get Object/Reference video number
+            idx = video_path.index(tag)
+            object_number = video_path[idx + len(tag):]
+            idx = object_number.index('/')
+            object_number = object_number[:idx]
+            # Create dir chain to store the features of the current video
+            dirs = os.path.dirname(video_path).split('/')
+            idx = dirs.index(table_name.replace(' ', '_'))
+            dir_features_to_save = dir_save
+            for i in dirs[idx:]:
+                dir_features_to_save = os.path.join(dir_features_to_save, i)
+            # For each layer to extract, create directory structure to save the features of the current video
+            for layer_name in layers_to_extract:
+                dir_features_to_save_layer = os.path.join(dir_features_to_save, layer_name)
+                if not os.path.isdir(dir_features_to_save_layer):
+                    os.makedirs(dir_features_to_save_layer)
+            # Name of the file with the feature is defined as: 'feat_[#name_of_the_video#]_#layer_name#_t#table_number#_#type_feature(ref or tar)#_vid#object_number#_frame#frame_number(starts at 0)#.npy'
+            prefix_file_name = f'feat_[%s]_#layerName#_t{table_number}_{type_feat}_vid{object_number}_frame_#frameNumber#.npy' % (
+                os.path.basename(video_path).replace('.avi', ''))
+            # Get features of the frame from all layers
+            generate_and_save_features(video_path,
+                                       dir_features_to_save,
+                                       prefix_file_name,
+                                       layers_to_extract,
+                                       resize_factor=resize_factor,
+                                       apply_post_pooling=apply_pooling)
 
-end = time.time()
-print('\nFinished process with %s seconds' % (end - start))
+    end = time.time()
+    print('\nFinished process with %s seconds' % (end - start))
